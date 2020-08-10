2e20176d9da8416429338181a5d52abae241013b
Christopher Brandt <cmb199@zips.uakron.edu>
2020-08-10 09:42:21 -0500

    Small updates to <stad> header file.

--- include/nola/stat.hxx
+++ include/nola/stat.hxx
@@ -14,19 +14,19 @@ namespace nola {
   //
   // Declarations
 
-  template <typename Real>
+  template <class Real>
   Real mean(Real* x, std::size_t n);
 
-  template <typename Real>
+  template <class Real>
   Real variance(Real* x, std::size_t n);
 
-  template <typename Real>
+  template <class Real>
   Real standard_deviation(Real* x, std::size_t n);
 
-  template <typename Real>
+  template <class Real>
   Real covariance(Real* x, Real* y, std::size_t n);
 
-  template <typename Real>
+  template <class Real>
   Real correlation(Real* x, Real* y, std::size_t n);
 
 
@@ -36,34 +36,34 @@ namespace nola {
 
   namespace detail {
 
-    template <typename Real>
+    template <class Real>
     Real mean_impl(Real* x, std::size_t) {
       //
       // compute mean
     }
 
-    template <typename Real>
+    template <class Real>
     Real variance_impl(Real* x, std::size_t) {
       //
-      // compute mean
+      // compute variance
     }
 
-    template <typename Real>
+    template <class Real>
     Real standard_deviation_impl(Real* x, std::size_t) {
       //
-      // compute mean
+      // compute standard deviation
     }
 
-    template <typename Real>
+    template <class Real>
     Real covariance_impl(Real* x, std::size_t) {
       //
-      // compute mean
+      // compute covariance
     }
 
-    template <typename Real>
+    template <class Real>
     Real correlation_impl(Real* x, std::size_t) {
       //
-      // compute mean
+      // compute correlation
     }
 
   } // namespace detail
@@ -73,27 +73,27 @@ namespace nola {
   //
   // C++17 Interfaces
 
-  template <typename Real>
+  template <class Real>>
   Real mean(Real* x, std::size_t n) {
     return nola::detail::mean_impl(x, n);
   }
 
-  template <typename Real>
+  template <class Real>
   Real variance(Real* x, std::size_t n) {
     return nola::detail::variance_impl(x, n);
   }
 
-  template <typename Real>
+  template <class Real>
   Real standard_deviation(Real* x, std::size_t n) {
     return nola::detail::standard_deviation_impl(x, n);
   }
 
-  template <typename Real>
+  template <class Real>
   Real covariance(Real* x, std::size_t n) {
     return nola::detail::covariance_impl(x, n);
   }
 
-  template <typename Real>
+  template <class Real>
   Real correlation(Real* x, std::size_t n) {
     return nola::detail::correlation_impl(x, n);
   }
