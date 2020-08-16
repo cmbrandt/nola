54d7b232267b2abce009ce26b9e8ae158f8bfbbe
Christopher Brandt <cmb199@zips.uakron.edu>
2020-08-16 09:54:04 -0500

    Updates from last night.

--- prototypes/root/brent.hxx
+++ prototypes/root/brent.hxx
@@ -17,11 +17,11 @@ namespace nola {
 
   template <class F, class Real>
   inline std::tuple<Real, std::size_t>
-  brent(F f, Real x0, Real x1);
+  brent(F const& f, Real x0, Real x1);
 
   template <class F, class Real>
   inline std::tuple<Real, std::size_t>
-  brent(F f, Real x0, Real x1, Real tol, std::size_t maxiter);
+  brent(F const& f, Real x0, Real x1, Real tol, std::size_t maxiter);
 
 
 
@@ -32,7 +32,7 @@ namespace nola {
 
     template <class F, class Real>
     inline std::tuple<Real, std::size_t>
-    brent_impl(F f, Real x0, Real x1, Real tol, std::size_t iter)
+    brent_impl(F const& f, Real x0, Real x1, Real tol, std::size_t iter)
     {
       Real fx0 = f(x0);
       Real fx1 = f(x1);
@@ -117,7 +117,7 @@ namespace nola {
 
   template <class F, class Real>
   inline std::tuple<Real, std::size_t>
-  brent(F f, Real x0, Real x1)
+  brent(F const& f, Real x0, Real x1)
   {
     Real tol = 1e-10;
     std::size_t maxiter = 100;
@@ -127,7 +127,7 @@ namespace nola {
 
   template <class F, class Real>
   inline std::tuple<Real, std::size_t>
-  brent(F f, Real x0, Real x1, Real tol, std::size_t maxiter)
+  brent(F const& f, Real x0, Real x1, Real tol, std::size_t maxiter)
   {
     return nola::detail::brent_impl(f, x0, x1, tol, maxiter);
   }
