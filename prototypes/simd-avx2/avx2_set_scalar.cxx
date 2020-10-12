6f2df158b65e79d8c33638453233ea5a9f3d2547
Christopher Brandt <cmb199@zips.uakron.edu>
2020-10-12 15:16:18 -0500

    First example complete.

--- prototypes/simd-avx2/avx2_set_scalar.cxx
+++ prototypes/simd-avx2/avx2_set_scalar.cxx
@@ -1,8 +1,7 @@
 // avx2_set_scalar.cxx
 
+#include <array>
 #include <iostream>
-#include <vector>
-//#include <immintrin.h>
 #include "nola_avx2.hxx"
 
 
@@ -12,15 +11,12 @@ int main()
 
   nola::v256f av = nola::avx2_set_scalar(a);
 
-  float v[8];
-  //std::vector<float> v{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+  std::array<float, 8> v;
 
-  nola::avx2_store(&v[0], av);
+  nola::avx2_store(v.data(), av);
 
   std::cout << "v = " << v[0] << " " << v[1] << " "
                       << v[2] << " " << v[3] << " "
                       << v[4] << " " << v[5] << " "
                       << v[6] << " " << v[7] << std::endl;
-
-
 }
