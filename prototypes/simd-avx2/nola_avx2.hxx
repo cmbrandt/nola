d84c8173a342510b42a416b41c8e1668328c70f1
Christopher Brandt <cmb199@zips.uakron.edu>
2020-10-12 19:55:16 -0500

    double precision avx2 simd examples working.. except for (1) *set_scalar.cxx and (2) *float_set_zero.cxx is an ambiguous redefinition (cannot overload on return type).

--- prototypes/simd-avx2/nola_avx2.hxx
+++ prototypes/simd-avx2/nola_avx2.hxx
@@ -22,7 +22,7 @@ using v256d = __m256d;
 
 // Single precision
 //inline v256f avx2_set_scalar(float a);
-inline v256f avx2_set_zero();
+//inline v256f avx2_set_zero();
 inline v256f avx2_broadcast(float const* addr);
 inline v256f avx2_load(float const* addr);
 inline void  avx2_store(float* addr, v256f a);
@@ -33,19 +33,19 @@ inline v256f avx2_div(v256f a, v256f b);
 inline v256f avx2_fma(v256f a, v256f b, v256f c);
 
 
-/*
+
 // Double precision
-inline v256d avx2_set_scalar(double a);
+//inline v256d avx2_set_scalar(double a);
 inline v256d avx2_set_zero();
 inline v256d avx2_broadcast(double const* addr);
 inline v256d avx2_load(double const* addr);
-inline v256d avx2_store(double* addr, v256d a);
+inline void  avx2_store(double* addr, v256d a);
 inline v256d avx2_add(v256d a, v256d b);
 inline v256d avx2_sub(v256d a, v256d b);
 inline v256d avx2_mul(v256d a, v256d b);
 inline v256d avx2_div(v256d a, v256d b);
 inline v256d avx2_fma(v256d a, v256d b, v256d c);
-*/
+
 
 //
 // Function implementations
@@ -54,8 +54,8 @@ inline v256d avx2_fma(v256d a, v256d b, v256d c);
 //inline v256f
 //avx2_set_scalar(float a) { return _mm256_set1_ps(a); }
 
-inline v256f
-avx2_set_zero() { return _mm256_setzero_ps(); }
+//inline v256f
+//avx2_set_zero() { return _mm256_setzero_ps(); }
 
 inline v256f
 avx2_broadcast(float const* addr) { return _mm256_broadcast_ss(addr); }
@@ -83,8 +83,35 @@ avx2_fma(v256f a, v256f b, v256f c) { return _mm256_fmadd_ps(a, b, c); }
 
 
 // Double precision
+//inline v256d
+//avx2_set_scalar(double a) { return _mm256_set1_pd(a); }
 
+inline v256d
+avx2_set_zero() { return _mm256_setzero_pd(); }
 
+inline v256d
+avx2_broadcast(double const* addr) { return _mm256_broadcast_sd(addr); }
+
+inline v256d
+avx2_load(double const* addr) { return _mm256_load_pd(addr); }
+
+inline void
+avx2_store(double* addr, v256d a) { _mm256_store_pd(addr, a); }
+
+inline v256d
+avx2_add(v256d a, v256d b) { return _mm256_add_pd(a, b); }
+
+inline v256d 
+avx2_sub(v256d a, v256d b) { return _mm256_sub_pd(a, b); }
+
+inline v256d
+avx2_mul(v256d a, v256d b) { return _mm256_mul_pd(a, b); }
+
+inline v256d
+avx2_div(v256d a, v256d b) { return _mm256_div_pd(a, b); }
+
+inline v256d
+avx2_fma(v256d a, v256d b, v256d c) { return _mm256_fmadd_pd(a, b, c); }
 
 
 } // namespace nola
