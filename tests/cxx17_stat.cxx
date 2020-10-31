f1e2bd34f735cc7a3c2ef65638d31c9657f9e2f9
Chris Brandt <cmb199@zips.uakron.edu>
2020-10-31 18:49:04 -0500

    C++17 Interface <stat> test complete.

--- tests/cxx17_stat.cxx
+++ tests/cxx17_stat.cxx
@@ -2,7 +2,6 @@
 // All rights reserved
 
 #include <cmath>
-//#include <iomanip>
 #include <iostream>
 #include <vector>
 #include <nola/cxx17/stat.hxx>
@@ -19,10 +18,10 @@ void cxx17_stat()
   fail = cxx17_variance_double(fail);
   fail = cxx17_standard_deviation_float(fail);
   fail = cxx17_standard_deviation_double(fail);
-  //fail = cxx17_covariance_float(fail);
-  //fail = cxx17_covariance_double(fail);
-  //fail = cxx17_correlation_float(fail);
-  //fail = cxx17_correlation_double(fail);
+  fail = cxx17_covariance_float(fail);
+  fail = cxx17_covariance_double(fail);
+  fail = cxx17_correlation_float(fail);
+  fail = cxx17_correlation_double(fail);
 
   if (fail == 0)
     std::cout << "\ncxx17_stat() passed with zero errors." << std::endl;
@@ -38,13 +37,13 @@ std::int32_t cxx17_arithmetic_mean_float(std::int32_t fail)
 {
   std::vector<float> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  float m = nola::stat::arithmetic_mean( x.size(), x.data() );
+  float r = nola::stat::arithmetic_mean( x.size(), x.data() );
 
-  if (m == 0.6f) {
+  if (r != 0.6f) {
     ++fail;
     std::cout << "\nERROR! nola::stat::arithmetic_mean( float )"
-              << "\nsoln  = " << 0.6
-              << "\nr1    = " << m << std::endl;
+              << "\nsoln  = " << 0.6f
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
@@ -55,13 +54,13 @@ std::int32_t cxx17_arithmetic_mean_double(std::int32_t fail)
 {
   std::vector<double> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  double m = nola::stat::arithmetic_mean( x.size(), x.data() );
+  double r = nola::stat::arithmetic_mean( x.size(), x.data() );
   
-  if (m == 0.6) {
+  if (r != 0.6) {
     ++fail;
     std::cout << "\nERROR! nola::stat::arithmetic_mean( double )"
               << "\nsoln  = " << 0.6
-              << "\nr1    = " << m << std::endl;
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
@@ -75,13 +74,13 @@ std::int32_t cxx17_variance_float(std::int32_t fail)
 {
   std::vector<float> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  float m = nola::stat::variance( x.size(), x.data() );
+  float r = nola::stat::variance( x.size(), x.data() );
 
-  if (m == 0.1f) {
+  if (r != 0.1f) {
     ++fail;
     std::cout << "\nERROR! nola::stat::variance( float )"
-              << "\nsoln  = " << 0.1
-              << "\nr1    = " << m << std::endl;
+              << "\nsoln  = " << 0.1f
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
@@ -92,13 +91,13 @@ std::int32_t cxx17_variance_double(std::int32_t fail)
 {
   std::vector<double> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  double m = nola::stat::variance( x.size(), x.data() );
+  double r = nola::stat::variance( x.size(), x.data() );
   
-  if (m == 0.1) {
+  if (r != 0.1) {
     ++fail;
     std::cout << "\nERROR! nola::stat::variance( double )"
               << "\nsoln  = " << 0.1
-              << "\nr1    = " << m << std::endl;
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
@@ -112,15 +111,15 @@ std::int32_t cxx17_standard_deviation_float(std::int32_t fail)
 {
   std::vector<float> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  float m = nola::stat::standard_deviation( x.size(), x.data() );
+  float r = nola::stat::standard_deviation( x.size(), x.data() );
 
-  bool b = std::abs( m - 0.3162) > 0.0001;
+  bool b = std::abs( r - 0.3162f) > 0.0001f;
 
-  if (!b) {
+  if (b) {
     ++fail;
     std::cout << "\nERROR! nola::stat::standard_deviation( float )"
-              << "\nsoln  = " << 0.3162
-              << "\nr1    = " << m << std::endl;
+              << "\nsoln  = " << 0.3162f
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
@@ -131,36 +130,102 @@ std::int32_t cxx17_standard_deviation_double(std::int32_t fail)
 {
   std::vector<double> x{ 0.2, 0.4, 0.6, 0.8, 1.0 };
 
-  double m = nola::stat::standard_deviation( x.size(), x.data() );
+  double r = nola::stat::standard_deviation( x.size(), x.data() );
   
-  bool b = std::abs( m - 0.3162) > 0.0001;
+  bool b = std::abs( r - 0.3162) > 0.0001;
 
-  if (!b) {
+  if (b) {
     ++fail;
     std::cout << "\nERROR! nola::stat::standard_deviation( double )"
               << "\nsoln  = " << 0.3162
-              << "\nr1    = " << m << std::endl;
+              << "\nr     = " << r << std::endl;
   }
 
   return fail;
 }
 
 
-
-
-
-
-
 //
 // Covariance
 
+std::int32_t cxx17_covariance_float(std::int32_t fail)
+{
+  std::vector<double> x{ 3.2, 1.4, 2.6, 0.8, 2.0 };
+  std::vector<double> y{ 2.1, 2.5, 2.3, 2.9, 2.7 };
+
+  float r = nola::stat::covariance( x.size(), x.data(), y.data() );
+
+  bool b = std::abs( r + 0.27f) > 0.001f;
+
+  if (b) {
+    ++fail;
+    std::cout << "\nERROR! nola::stat::covariance( float )"
+              << "\nsoln  = " << -0.27f
+              << "\nr     = " << r << std::endl;
+  }
+
+  return fail;
+}
 
 
+std::int32_t cxx17_covariance_double(std::int32_t fail)
+{
+  std::vector<double> x{ 3.2, 1.4, 2.6, 0.8, 2.0 };
+  std::vector<double> y{ 2.1, 2.5, 2.3, 2.9, 2.7 };
 
+  double r = nola::stat::covariance( x.size(), x.data(), y.data() );
 
+  bool b = std::abs( r + 0.27) > 0.001;
 
+  if (b) {
+    ++fail;
+    std::cout << "\nERROR! nola::stat::covariance( double )"
+              << "\nsoln  = " << -0.27
+              << "\nr     = " << r << std::endl;
+  }
+
+  return fail;
+}
 
 
 //
 // Correlation
 
+std::int32_t cxx17_correlation_float(std::int32_t fail)
+{
+  std::vector<float> x{ 3.2, 1.4, 2.6, 0.8, 2.0 };
+  std::vector<float> y{ 2.1, 2.5, 2.3, 2.9, 2.7 };
+
+  float r = nola::stat::correlation( x.size(), x.data(), y.data() );
+
+  bool b = std::abs( r + 0.9f) > 0.01f;
+
+  if (b) {
+    ++fail;
+    std::cout << "\nERROR! nola::stat::correlation( float )"
+              << "\nsoln  = " << -0.9f
+              << "\nr     = " << r << std::endl;
+  }
+
+  return fail;
+}
+
+
+std::int32_t cxx17_correlation_double(std::int32_t fail)
+{
+  std::vector<double> x{ 3.2, 1.4, 2.6, 0.8, 2.0 };
+  std::vector<double> y{ 2.1, 2.5, 2.3, 2.9, 2.7 };
+
+  double r = nola::stat::correlation( x.size(), x.data(), y.data() );
+  
+  bool b = std::abs( r + 0.9) > 0.01;
+
+  if (b) {
+    ++fail;
+    std::cout << "\nERROR! nola::stat::correlation( double )"
+              << "\nsoln  = " << -0.9
+              << "\nr     = " << r << std::endl;
+  }
+
+  return fail;
+}
