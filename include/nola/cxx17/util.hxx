e6748a44e49bed355df2805d3c696ba4d154e800
Chris Brandt <cmb199@zips.uakron.edu>
2020-11-08 19:28:32 -0600

    Added C++17 <util.hxx> header file.

---
+++ include/nola/cxx17/util.hxx
@@ -0,0 +1,22 @@
+// Copyright (c) 2019-2021 Christopher M. Brandt
+// All rights reserved
+
+#ifndef NOLA_CXX17_UTIL_HXX
+#define NOLA_CXX17_UTIL_HXX
+
+
+namespace nola
+{
+namespace util
+{
+
+
+
+
+
+
+} // namespace util
+} // namespace nola
+
+
+#endif
