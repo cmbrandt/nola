0e5324a38512502ff228ef496259d96c05f0a2ea
Chris Brandt <cmb199@zips.uakron.edu>
2020-11-12 11:08:48 -0600

    Updating C++17 BLAS matrix_vector_product() example.

--- examples/cxx17/cxx17_blas_matrix_vector_product.cxx
+++ examples/cxx17/cxx17_blas_matrix_vector_product.cxx
@@ -7,7 +7,7 @@
 #include <nola/cxx17/blas.hxx>
 #include <nola/cxx17/util.hxx>
 
-/*
+
 int main()
 {
   std::cout << "\nMatrix Vector Product C++17 Interface Example." << std::endl;
@@ -20,7 +20,7 @@ int main()
   std::vector<double> y{ 1.0, 1.0, 1.0 };
 
   // Specify form of op(A)
-  auto NoTrans = nola::blas::no_transpose;
+  using nola::blas::no_transpose;
 
   // Dimensions of matrix A
   std::int32_t m = 3; // rows
@@ -31,7 +31,7 @@ int main()
   double beta  = 0.25;
 
   // Compute operation y := alpha*op(A)*x + beta*y
-  nola::blas::matrix_vector_product(NoTrans, m, n, alpha, a.data(),
+  nola::blas::matrix_vector_product(no_transpose, m, n, alpha, a.data(),
                                     x.data(), beta, y.data() );
 
   // Display result
@@ -41,42 +41,11 @@ int main()
   //  4.75 6.25 7.75
   // ]
 }
+
+/*
+
+g++-10 -Wall -Wpedantic -Wextra -std=c++17 -I \
+~/projects/nola/include cxx17_blas_matrix_vector_product.cxx \
+../../lib/libnola.a -lgfortran -o prod.exe
+
 */
-
-
-int main()
-{
-
-  // USE THIS FOR MY TEST !!!
-
-  std::cout << "\nMatrix Vector Product C++17 Interface Example." << std::endl;
-
-  // (Flattened) matrix of values
-  std::vector<double> a{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
-
-  // Vectors of values
-  std::vector<double> x{ 1.0, 2.0 };
-  std::vector<double> y{ 1.0, 1.0, 1.0 };
-
-  // Specify form of op(A)
-  auto Trans = nola::blas::transpose;
-
-  // Dimensions of matrix A
-  std::int32_t m = 3; // rows
-  std::int32_t n = 2; // cols
-
-  // Scalar coefficients
-  double alpha = 0.5;
-  double beta  = 0.25;
-
-  // Compute operation y := alpha*op(A)*x + beta*y
-  nola::blas::matrix_vector_product(Trans, m, n, alpha, a.data(),
-                                    y.data(), beta, x.data() );
-
-  // Display result
-  nola::util::print_vector("\ny", x.size(), x.data(), 3, 4);
-
-  // Vector Y = [
-  //  
-  // ]
-}
