3a84e5a1935f23dda194356497e3efb01f33c275
Chris Brandt <cmb199@zips.uakron.edu>
2020-11-14 21:08:03 -0600

    ExectionPolicy classes, instances, and type trait working correctly.

--- prototypes/exec-impl/ex_types.cxx
+++ prototypes/exec-impl/ex_types.cxx
@@ -1,11 +1,23 @@
 // Checking the type information for each exec argument
 
 #include <iostream>
-//#include "display_type.hxx"
+#include "exec.hxx"
+
 
 int main()
 {
-  
+  std::cout << "\nint                                      "
+            << nola::exec::is_execution_policy_v<int>
+            << "\nnola::exec::sequential_policy            "
+            << nola::exec::is_execution_policy_v<nola::exec::sequential_policy>
+            << "\nnola::exec::parallel_policy              "
+            << nola::exec::is_execution_policy_v<nola::exec::parallel_policy>
+            << "\nnola::exec::parallel_unsequential_policy "
+            << nola::exec::is_execution_policy_v<nola::exec::parallel_unsequential_policy>
+            << "\nnola::exec::unsequential_policy          "
+            << nola::exec::is_execution_policy_v<nola::exec::unsequential_policy>
+            << "\nvoid                                     "
+            << nola::exec::is_execution_policy_v<void><< std::endl;  
 
   std::cout << "\nDone." << std::endl;
 }
